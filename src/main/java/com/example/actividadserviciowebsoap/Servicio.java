863cdcc212562167af1f6377994ea5dd174ebb0d
ThayliAreliVillegas112 <20203tn064@utez.edu.mx>
2021-09-23 18:48:59 -0500

    Septimo intento

---
+++ src/main/java/com/example/actividadserviciowebsoap/Servicio.java
@@ -0,0 +1,19 @@
+package com.example.actividadserviciowebsoap;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.ws.Endpoint;
+
+    @WebService(name="Servicio", targetNamespace="utez")
+    @SOAPBinding(style=SOAPBinding.Style.RPC)
+    public class Servicio {
+        @WebMethod( operationName="devuelveMensaje")
+        public String devuelveMensaje( @WebParam String valor ){
+            return "Hola " + valor;
+        }
+        public static void main( String[] args ){
+            Endpoint.publish( "http://localhost:7777/Servicio", new Servicio( ) );
+        }
+    }
